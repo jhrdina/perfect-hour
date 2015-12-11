f793cf1b11a1f9e47d23cc0edd51922b491c1f2d
Jan Hrdina <jan.hrdka@gmail.com>
2015-12-11 12:36:11 +0100

    SettingsActivity added

--- app/src/main/java/cz/hrdinajan/perfecthour/MainActivity.java
+++ app/src/main/java/cz/hrdinajan/perfecthour/MainActivity.java
@@ -1,5 +1,6 @@
 package cz.hrdinajan.perfecthour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -46,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
